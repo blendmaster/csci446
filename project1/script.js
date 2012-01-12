70d3001b9281c494c6ae92ca66ab2d32f3fc6be6
Steven Ruppert <blendmaster@gmail.com>
2012-01-11 20:34:35 -0700

    split css and javascript to separate files

---
+++ project1/script.js
@@ -0,0 +1,10 @@
+//html5 validity api all up ins
+var validate = function () { 
+	this.setCustomValidity((this.validity.valueMissing) ? this.dataset.errorMessage : "");
+};
+var feel = document.getElementById('feel')['current-feel'];
+feel.addEventListener('input', validate);
+feel.addEventListener('invalid', validate);
+
+//prevent form submission (page reload)
+document.getElementById('feel').addEventListener('submit', function(e) { e.preventDefault(); });
