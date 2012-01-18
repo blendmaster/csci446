db283349690702a688068cd983ac3ce87e3f6790
Steven Ruppert <blendmaster@gmail.com>
2012-01-18 08:41:53 -0700

    added project3 base files

---
+++ project03/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Project03::Application.config.secret_token = '57c3eacbe574f93b25f31313e8e80f2806f15e67585f5677eb0c16e05bde9f30467f866f3890dcb7f547ccc1e993fa583de0d36879ef5471ad3f1e4fac2ce5b8'
