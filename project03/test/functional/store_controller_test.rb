91bc911a442eeffa217e96f2e749863d187102b6
Steven Ruppert <blendmaster@gmail.com>
2012-01-18 10:32:28 -0700

    finished store controller test

--- project03/test/functional/store_controller_test.rb
+++ project03/test/functional/store_controller_test.rb
@@ -5,6 +5,7 @@ class StoreControllerTest < ActionController::TestCase
   test "should get index" do
     get :index
     assert_response :success
+	assert_select '.price', /\$[,\d]+\.\d\d/
   end
 
 end
