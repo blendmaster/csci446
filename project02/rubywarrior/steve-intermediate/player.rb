ed963e1c5062a5e829dc2a91593c1d8e16bf68e5
Steven Ruppert <blendmaster@gmail.com>
2012-01-18 04:02:10 -0700

    finished level 1

--- project02/rubywarrior/steve-intermediate/player.rb
+++ project02/rubywarrior/steve-intermediate/player.rb
@@ -1,5 +1,21 @@
 class Player
   def play_turn(warrior)
-    # add your code here
+	  if warrior.health < 3
+		  warrior.rest!
+		  return
+	  end
+	  if danger(warrior)
+		  warrior.attack! danger(warrior)
+		  return
+	  end
+	  warrior.walk! warrior.direction_of_stairs
+  end
+  def danger(warrior)
+	  [:left, :right, :forward, :backward].each do |direction| 
+		  if warrior.feel(direction).enemy?
+			  return direction
+		  end
+	  end
+	  return false
   end
 end
