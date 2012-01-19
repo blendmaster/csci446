9915933f26bc2ec707c6ca7cbb4ea3132b03762b
Steven Ruppert <blendmaster@gmail.com>
2012-01-18 21:58:20 -0700

    rewrote product views in haml and formtastic

--- project03/app/models/product.rb
+++ project03/app/models/product.rb
@@ -1,6 +1,6 @@
 # -*- encoding : utf-8 -*-
 class Product < ActiveRecord::Base
-	validates :title, :description, :image_url, presence: true
+	validates :title, :description, :image_url, :price, presence: true
 	validates :price, numericality: {greater_than_or_equal_to: 0.01}
 	validates :image_url, format: {
 		with: %r{\.(gif|jpg|png)$}i,
