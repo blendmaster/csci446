fe8b5fa6b3c8a6e14d561a4d703795a53d1ff2d2
Steven Ruppert <blendmaster@gmail.com>
2012-01-19 20:58:45 -0700

    implemented article edits functionality

--- project04/app/models/article.rb
+++ project04/app/models/article.rb
@@ -1,4 +1,8 @@
 class Article < ActiveRecord::Base
+
 	validates :title, :author, :body, presence: true
 	validates :author, format: { without: /pat/i, message: "Your kind isn't allowed here, Pat!" }
+
+	attr_protected :edits
+	before_update {|article| article.edits += 1}
 end
