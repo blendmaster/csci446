c31ce75956f888e1a8da7b9623dd5aa82823af0b
Steven Ruppert <blendmaster@gmail.com>
2012-01-19 20:35:09 -0700

    wrote some bitchin article unit tests

--- project04/test/unit/article_test.rb
+++ project04/test/unit/article_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class ArticleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	test "article fields are present" do
+		[:no_title, :no_author, :no_body].each do |a|
+			assert articles(a).invalid?, "article with #{a} is valid!"
+		end
+	end
+
+	test "no pat articles" do
+		assert articles(:one).valid?, "Article not by pat is invalid!"
+		assert articles(:by_pat).invalid?, "Article by pat is valid!"
+	end
 end
