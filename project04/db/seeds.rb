1a192492bab06fbec6db8db884d4b93791bf69c3
Steven Ruppert <blendmaster@gmail.com>
2012-01-19 23:51:28 -0700

    added seed articles for development

--- project04/db/seeds.rb
+++ project04/db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+#
+require 'wordy'
+
+def words num
+	Wordy.words(num).map(&:capitalize).join(" ")
+end
+
+100.times do
+	Article.create title: words(3), author: words(2), body: Wordy.paragraphs(3).join("\n\n")
+end
