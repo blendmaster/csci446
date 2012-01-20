1a3f2e6ad156b490c7b11c752b2bf48c98b8de5a
Steven Ruppert <blendmaster@gmail.com>
2012-01-19 20:45:26 -0700

    passed all articles validation tests

--- project04/app/models/article.rb
+++ project04/app/models/article.rb
@@ -1,2 +1,4 @@
 class Article < ActiveRecord::Base
+	validates :title, :author, :body, presence: true
+	validates :author, format: { without: /pat/i, message: "Your kind isn't allowed here, Pat!" }
 end
