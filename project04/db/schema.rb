4c3c9707943c4f599ba8f70f4b3998ad1f5ac196
Steven Ruppert <blendmaster@gmail.com>
2012-02-20 02:03:17 -0700

    wrote lame functional test to get my ratio up

--- project04/db/schema.rb
+++ project04/db/schema.rb
@@ -11,6 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 20120120031613) do
+
+  create_table "articles", :force => true do |t|
+    t.string   "title"
+    t.string   "author"
+    t.text     "body"
+    t.integer  "edits",      :default => 0, :null => false
+    t.datetime "created_at",                :null => false
+    t.datetime "updated_at",                :null => false
+  end
 
 end
--- project04/test/functional/articles_controller_test.rb
+++ project04/test/functional/articles_controller_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class ArticlesControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	def setup
+		@article = articles :one
+	end
+
+	test "edit article redirected to previous path" do
+		[articles_url, article_url(@article)].each do |url|
+			post :update, id:@article.id, article: { title: 'title', author: 'author', body: 'body' }, previous_page: url
+			assert_redirected_to url, message: "not redirected to #{url}!"
+		end
+	end
+		
 end
