4bb8446b5c80a55a5d7bec5fd988a1bf8d49fe2b
Steven Ruppert <blendmaster@gmail.com>
2012-02-24 18:48:11 -0700

    cleaned up icon handling in stylesheets

--- project05/app/helpers/application_helper.rb
+++ project05/app/helpers/application_helper.rb
@@ -1,10 +1,14 @@
 # encoding: UTF-8
 module ApplicationHelper
-	def title title
-		content_for(:title) { "Articl.es—#{title}" }
-	end
-	
-	def subtitle subtitle
-		content_for(:subtitle) { subtitle }
-	end
+  def title title
+    content_for(:title) { "Articl.es—#{title}" }
+  end
+
+  def subtitle subtitle
+    content_for(:subtitle) { subtitle }
+  end
+
+  def icon_to text, icon, link, options
+    link_to text, link, options.merge(:class => "#{options[:class]} icon", :'data-icon' => icon)
+  end
 end
