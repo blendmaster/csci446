d6d9837dba639f2dc275380cde6e45dc87e0e298
Steven Ruppert <blendmaster@gmail.com>
2012-03-29 16:33:39 -0600

    revamped my clever little controls method

--- project06/app/controllers/application_controller.rb
+++ project06/app/controllers/application_controller.rb
@@ -1,71 +1,54 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-
-  def index
-    @games = Game.paginate page: params[:page]
-  end
   
   # let's do some fancy metaprogramming
   def self.controls models, options = {}
     model = models.to_s.singularize
     model_instances = "@#{models}"
     model_instance = "@#{model}"
-    constant = model.capitalize.constantize
+    constant = model.camelcase.constantize
 
     options[:only] ||= [:index, :show, :new, :create, :edit, :update, :destroy]
-
-    before_filter only: [:show, :edit, :update] do
-      instance_variable_set model_instance, constant.find(params[:id])
-    end
-
-    if options[:only].include? :index
-      define_method :index do
-        instance_variable_set model_instances, constant.paginate(page: params[:page])
-      end
-    end
-
-    if options[:only].include? :show
-      define_method :show do
-      end
-    end
-
-    if options[:only].include? :new
-      define_method :new do
-        instance_variable_set model_instance, constant.new
-      end
-    end
+    options[:except] ||= []
     
-    if options[:only].include? :create
-      define_method :create do
+    actions = { # looks like javascript, huh
+      index: lambda do
+        instance_variable_set model_instances, constant.paginate(page: params[:page])
+      end,
+      show: lambda {}, # don't really need to do anything
+      new: lambda do
+        instance_variable_set model_instance, constant.new
+      end,
+      create: lambda do
         instance_variable_set model_instance, constant.new(params[model])
         if instance_variable_get(model_instance).save
           redirect_to instance_variable_get(model_instance), notice: "#{model.capitalize} successfully created!"
         else
           render :new
         end
-      end
-    end
-
-    if options[:only].include? :edit
-      define_method :edit do
-      end
-    end
-
-    if options[:only].include? :update
-      define_method :update do
+      end,
+      edit: lambda {},
+      update: lambda do
         if instance_variable_get(model_instance).update_attributes(params[model])
           redirect_to instance_variable_get(model_instance), notice: "#{model.capitalize} sucessfully updated!"
         else
           render :edit
         end
-      end
-    end
-
-    if options[:only].include? :destroy
-      define_method :destroy do
+      end,
+      destroy: lambda do
         constant.find(params[:id]).destroy
         redirect_to send("#{models}_url".to_sym), notice: "#{model.capitalize} sucessfully purged!"
       end
+    }
+
+    # set @object for applicable methods
+    before_filter only: [:show, :edit, :update] do
+      instance_variable_set model_instance, constant.find(params[:id])
+    end
+    
+    # add methods
+    (options[:only] - options[:except]).each do |method|
+      define_method method, actions[method]
     end
   end
 end
