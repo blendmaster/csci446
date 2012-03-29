0d1ac42f4991a26458cec5925e6ee6562735aa40
Steven Ruppert <blendmaster@gmail.com>
2012-03-28 22:26:36 -0600

    added game model

---
+++ project06/app/controllers/games_controller.rb
@@ -0,0 +1,3 @@
+class GamesController < ApplicationController
+  controls :games
+end
---
+++ project06/app/models/game.rb
@@ -0,0 +1,9 @@
+class Game < ActiveRecord::Base
+  attr_accessible :title, :rating
+  validates_presence_of :title
+  enum_attr :rating, %w[Amazing Good Meh Horrible]
+  
+  default_scope order: 'created_at DESC'
+  
+  @per_page = 10
+end
