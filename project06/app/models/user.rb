7c8da82979c4d6c0ec04b3fa889a77d46327c3b6
Steven Ruppert <blendmaster@gmail.com>
2012-03-29 18:52:27 -0600

    added extra attributes to user
    sometimes devise is annoying

--- project06/app/models/user.rb
+++ project06/app/models/user.rb
@@ -5,5 +5,14 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :username, :first_name, :last_name, :photo
+  
+  validates :username, uniqueness: true, length: { minimum: 6 }
+  validates_presence_of :username, :first_name, :last_name 
+  
+  has_attached_file :photo, styles: { thumb: '150x150' }
+  
+  def to_s
+    "#{first_name} #{last_name}"
+  end
 end
