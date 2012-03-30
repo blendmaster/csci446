9a9c8142a9787c5db4bb81417a6a0e2d54a8743c
Steven Ruppert <blendmaster@gmail.com>
2012-03-29 19:15:06 -0600

    added recaptcha to registration

--- project06/config/routes.rb
+++ project06/config/routes.rb
@@ -1,5 +1,5 @@
 Gamez::Application.routes.draw do
-  devise_for :users, path: 'members', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
+  devise_for :users, path: 'members', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }, controllers: { registrations: "registrations"}
   
   get 'members/me' => 'settings#edit'
   put 'members/me' => 'settings#update'
