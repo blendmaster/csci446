fc48f923fdc1f13d34a7fbef80ea898aaa527694
Steven Ruppert <blendmaster@gmail.com>
2012-04-02 19:00:08 -0600

    added roles crud

--- project06/app/controllers/admin/roles_controller.rb
+++ project06/app/controllers/admin/roles_controller.rb
@@ -1,2 +1,4 @@
 class Admin::RolesController < ApplicationController
+  authorize_resource
+  controls :roles, as: :admin, redirect_url: :admin_roles_url
 end
--- project06/app/models/ability.rb
+++ project06/app/models/ability.rb
@@ -2,13 +2,15 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    can :manage, :all if user.admin?
-    if user.member?
-      can :manage, Game # do |g|
-      #  u == user # not sure why user: user hash isn't working
-      # end
-      can :update, User do |u|
-        u == user
+    if user
+      can :manage, :all if user.admin?
+      if user.member?
+        can :manage, Game # do |g|
+        #  u == user # not sure why user: user hash isn't working
+        # end
+        can :update, User do |u|
+          u == user
+        end
       end
     end
   end
